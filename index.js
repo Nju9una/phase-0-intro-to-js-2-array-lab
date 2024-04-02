6b09213602087c73a001bd96658bf416818361cb
Patrick Njuguna <mpatricknjuguna02@gmail.com>
2024-04-02 11:33:43 +0300

    intro to arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Write your solution here!
+// Define the cats array
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Function to append a cat to the end of the cats array 
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+// Function to prepend a cat to the beginning of the cats array 
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+// Function to remove the last cat from the cats array 
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+// Function to remove the first cat from the cats array 
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Function to append a cat to the cats array and return a new array 
+function appendCat(name) {
+    return cats.concat(name);
+}
+
+// Function to prepend a cat to the cats array and return a new array 
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+// Function to remove the last cat in the cats array and return a new array 
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+
+// Function to remove the first cat from the cats array and return a new array
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+// Export functions
+module.exports = {
+    destructivelyAppendCat,
+    destructivelyPrependCat,
+    destructivelyRemoveLastCat,
+    destructivelyRemoveFirstCat,
+    appendCat,
+    prependCat,
+    removeLastCat,
+    removeFirstCat
+};
